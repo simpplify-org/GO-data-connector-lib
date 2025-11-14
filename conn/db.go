2c1b10a4dcb2cf5d7815556e4433c44e0c6a6f5c
MARIA LUIZA <machadosilvam42@gmail.com>
2025-11-14 16:48:56 -0300

    added validate

--- conn/db.go
+++ conn/db.go
@@ -4,25 +4,28 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 )
 
 type Config struct {
-	DBDriver   string
-	DBUser     string
-	DBPassword string
-	DBHost     string
-	DBPort     string
-	DBDatabase string
-	DBSSLMode  string
-	AppName    string
+	DBDriver        string
+	DBUser          string
+	DBPassword      string
+	DBHost          string
+	DBPort          string
+	DBDatabase      string
+	DBSSLMode       string
+	AppName         string
+	MaxOpenConns    int
+	MaxIdleConns    int
+	ConnMaxLifetime time.Duration
 }
 
 func NewConn(config Config) (*sql.DB, error) {
 	var db *sql.DB
 
-	if config.AppName == "" {
-		config.AppName = "data-connector-lib"
-	}
+	config.validate()
+
 	dsn := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s application_name=%s",
 		config.DBHost,
@@ -39,6 +42,24 @@ func NewConn(config Config) (*sql.DB, error) {
 		log.Fatal("Cannot connect to db:", err)
 		return nil, err
 	}
+	db.SetMaxOpenConns(config.MaxOpenConns)
+	db.SetMaxIdleConns(config.MaxIdleConns)
+	db.SetConnMaxLifetime(config.ConnMaxLifetime)
 
 	return db, nil
+}
+
+func (c *Config) validate() {
+	if c.AppName == "" {
+		c.AppName = "data-connector-lib"
+	}
+	if c.MaxOpenConns <= 0 {
+		c.MaxOpenConns = 20
+	}
+	if c.MaxIdleConns <= 0 {
+		c.MaxIdleConns = 10
+	}
+	if c.ConnMaxLifetime <= 0 {
+		c.ConnMaxLifetime = time.Hour
+	}
 }
